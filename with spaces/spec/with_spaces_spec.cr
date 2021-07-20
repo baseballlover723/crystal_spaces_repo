3e7c51199d007e38876a5100cbd35f72fca4d049
Philip Ross <dev@baseballlover723.com>
2021-07-19 23:38:54 -0700

    show __DIR__ values at runtime and compiletime

--- with spaces/spec/with_spaces_spec.cr
+++ with spaces/spec/with_spaces_spec.cr
@@ -4,6 +4,8 @@ describe WithSpaces do
   # TODO: Write tests
 
   it "can print Myhtml.version" do
+    {% puts "compiletime __DIR__: #{__DIR__}" %}
+    puts "runtime __DIR__: #{__DIR__}"
     puts Myhtml.version
     true.should eq(true)
   end
